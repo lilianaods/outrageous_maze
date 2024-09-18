4ef573cff5eac8456ab133dfa8acc804fac49d10
Liliana Silva <lilianaods@outlook.com>
2024-09-18 19:17:04 -0400

    Add endpoint to retrieve posts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import fastify from "fastify";
 dotenv.config();
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.get("/posts", async (req, res) => {
+  return await prisma.post.findMany({ select: { id: true, title: true } });
+});
 
 app.listen({ port: process.env.PORT });
