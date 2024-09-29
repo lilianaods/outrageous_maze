13a50ae96b3756e2ecf453566556c6024561e4c3
Liliana Silva <lilianaods@outlook.com>
2024-09-29 07:45:01 -0400

    Add endpoint for updating comments

--- server/server.js
+++ server/server.js
@@ -78,6 +78,30 @@ app.post("/posts/:id/comments", async (req, res) => {
   );
 });
 
+app.put("/posts/:postId/comments/:commentId", async (req, res) => {
+  if (req.body.message === "" || !req.body.message) {
+    return res.send(app.httpErrors.badRequest("Message is required"));
+  }
+  const { userId } = await prisma.comment.findUnique({
+    where: { id: req.params.commentId },
+    select: { userId: true },
+  });
+  if (userId !== req.cookies.userId) {
+    return res.send(
+      app.httpErrors.unauthorized(
+        "You do not have permission to edit this message"
+      )
+    );
+  }
+  return await commitToDb(
+    prisma.comment.update({
+      where: { id: req.params.commentId },
+      data: { message: req.body.message },
+      select: { message: true },
+    })
+  );
+});
+
 app.listen({ port: process.env.PORT });
 
 const commitToDb = async (promise) => {
